97fb2b84570ac0b8cf2e4901abf9da41a6c2db70
visualstudio <visualstudio@122-PC04>
2019-02-06 12:02:51 -0500

    commit1111

--- CO732156/Program.cs
+++ CO732156/Program.cs
@@ -7,6 +7,9 @@ namespace CO732156
         static void Main(String[] args)
         {
             LinkedList a = new LinkedList();
+            a.run();
+            a.WalkOverTheList();
+            a.ReverseTheList();
         }
     }
 
@@ -21,6 +24,7 @@ namespace CO732156
         public String DogName;
         public String DogBreed;
         public Dog nextDog;
+        public Dog previousDog;
 
     }
 
@@ -28,16 +32,22 @@ namespace CO732156
     { 
         public Dog current = new Dog();
         public Dog Head = new Dog();
+        public Dog Tail = new Dog();
         public Dog Peanut = new Dog();
         public Dog Fifi = new Dog();
         public Dog Jordan = new Dog();
         public Dog Fido = new Dog();
+        public Dog Roy = new Dog();
+        public Dog Coco = new Dog();
 
         public void run()
         {
             Head = Peanut;
+            Tail = Fido;
             Peanut.DogName = "Peanut";
             Peanut.DogBreed = "Bichon";
+            Peanut.nextDog = Fifi;
+            Fifi.previousDog = 
 
             Peanut.nextDog = Fifi;
 
@@ -52,6 +62,16 @@ namespace CO732156
             Fido.DogName = "Fido";
             Fido.DogBreed = "Beagle";
             Fido.nextDog = null;
+
+            Coco.DogName = "Coco";
+            Coco.DogBreed = "Border Colli";
+            Coco.nextDog = Jordan;
+            Coco.previousDog = Fifi;
+
+            Roy.DogName = "Roy";
+            Roy.DogBreed = "Tail";
+            Roy.nextDog = 
+
         }
         public void WalkOverTheList()
         {
